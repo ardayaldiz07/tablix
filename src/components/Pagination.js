844e93b6e97d08d96870b633b28986144879d550
ardayaldiz07 <ardayaldiz07@gmail.com>
2024-10-15 17:09:50 +0300

    Updates

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -45,21 +45,61 @@ export default class Pagination {
         return select;
     }
 
+    renderPageNumbers() {
+        const wrapper = document.createElement('div');
+        wrapper.classList.add('tx-page-numbers');
+        wrapper.style.display='flex';
+
+
+        const totalPages = 20;
+        const visiblePages = 5;
+
+        let startPage = Math.max(0, this.currentPage - Math.floor(visiblePages / 2));
+        let endPage = Math.min(totalPages - 1, startPage + visiblePages - 1);
+
+        for (let i = startPage; i <= endPage; i++) {
+            const pageButton = document.createElement('p');
+            pageButton.style.margin='5px';
+            pageButton.style.cursor ='pointer';
+            pageButton.innerText = i + 1;
+            pageButton.classList.add('tx-page-number');
+
+            if (i === this.currentPage) {
+                pageButton.classList.add('active');
+            }
+
+            pageButton.addEventListener('click', () => {
+                this.currentPage = i;
+                this.callback(this.currentPage, this.selectedLength);
+                this.renderPageNumbers();
+            });
+
+            wrapper.appendChild(pageButton);
+        }
+
+        return wrapper;
+    }
+
     renderButtons(){
         const wrapper = document.createElement('div');
-
+        
         //Sıradaki
         const nextButton = document.createElement('button');
         nextButton.innerHTML = 'Sıradaki';
         nextButton.addEventListener("click", ()=>{
+            
             if(this.currentPage<this.paginatedData.totalPages-1){
+                
                 this.currentPage++;
                 this.callback(this.currentPage, this.selectedLength);
             }
             // else{
-            //     nextButton.classList.toggle('tx-disabled');
+            //     nextButton.classList.toggle('tx-btn-disabled');
             // }
         });
+        //Sıradaki
+
+
 
         //Önceki
         const prevButton = document.createElement('button');
@@ -71,12 +111,39 @@ export default class Pagination {
                 this.callback(this.currentPage, this.selectedLength);        
             }
             // else{
-            //     nextButton.classList.toggle('tx-disabled');
+            //     prevButton.classList.add('tx-btn-disabled');
             // }
         });
+        //Önceki
 
+     
         wrapper.appendChild(prevButton);
         wrapper.appendChild(nextButton);
+
+        // Ekstra Butonlar
+        if(this.options.pagination.enabledExtra){
+            const goToStartButton = document.createElement('button');
+            const extraButtonsWrapper = document.createElement('div');
+            extraButtonsWrapper.className ='tx-extra-buttons';
+
+            goToStartButton.innerText = 'Başa dön';
+            goToStartButton.addEventListener('click', () =>{
+                this.callback(0,this.selectedLength);
+            });
+
+            const goToEndButton = document.createElement('button');
+            goToEndButton.innerText = 'Sona git';
+    
+            goToEndButton.addEventListener('click', ()=>{
+                this.callback(this.paginatedData.totalPages-1,this.selectedLength);
+            });
+
+            extraButtonsWrapper.appendChild(goToStartButton);
+            extraButtonsWrapper.appendChild(goToEndButton);
+
+            wrapper.appendChild(extraButtonsWrapper);
+        }
+        // Ekstra Butonlar
 
         return wrapper;
     }
@@ -84,11 +151,17 @@ export default class Pagination {
     render() {
         const wrapper = document.createElement('div');
         const buttons = this.renderButtons();
+        const lengthFilter = this.lengthFilter();
+        const pageNumbers = this.renderPageNumbers();
+
         wrapper.className = "tx-pagination";
 
 
-        wrapper.appendChild(this.lengthFilter());
+        wrapper.appendChild(lengthFilter);
         wrapper.appendChild(buttons);
+        wrapper.appendChild(pageNumbers);
+
+        
         this.container.appendChild(wrapper);
 
         this.callback(this.currentPage, this.selectedLength);
